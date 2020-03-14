d765923d3224d66c0fa7ca932ec772aa98ba2a4e
GusGo <gus.gonzalez.15@gmail.com>
2020-03-14 12:05:53 -0400

    change html page on successful signup

--- public/js/signup.js
+++ public/js/signup.js
@@ -29,7 +29,7 @@ $(document).ready(function () {
       password: password
     })
       .then(data => {
-        // window.location.replace("/members");
+        window.location.replace("/members");
         console.log(data);
         // If there's an error, handle it by throwing up a bootstrap alert
       })
