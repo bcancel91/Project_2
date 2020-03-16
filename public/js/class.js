fe5ae7e4198b7960a24e7080334374438ad4fd10
Daria <dariagnaumova@gmail.com>
2020-03-16 11:10:35 -0400

    Add class to the db  from the class form works

--- public/js/class.js
+++ public/js/class.js
@@ -1,53 +1,53 @@
 $(document).ready(function () {
-    // Getting references to our form and input
-    let classForm = $(".option-add");
-    let topicInput = $("input#topic-input");
-    let descriptInput = $("input#description-input");
-    let dateInput = $("input#datepicker");
-    let timeInput = $("input#time");
-    let capacityInput = $("input#capacity-input");
-    let priceInput = $("input#price-input");
-  
-    // When the signup button is clicked, we validate the email and password are not blank
-    classForm.on("submit", function (event) {
-      event.preventDefault();
-  
-      let classData = {
-        topic: topicInput.val().trim(),
-        description: descriptInput.val().trim(),
-        date: dateInput.val().trim(),
-        time: timeInput.val().trim(),
-        capacity: capacityInput.val().trim(),
-        price: priceInput.val().trim(),
-      };
-  
-      console.log("class: ", classData);
-  
-      if (!classData.topic || !classData.description || !classData.date
-        || !classData.time || !classData.capacity || !classData.price === null) {
-        return;
-      }
-      createClass(userData.topic, classData.description, classData.date, classData.time, classData.capacity, classData.price);
+  // Getting references to our form and input
+  let classForm = $(".option-add");
+  let topicInput = $("input#topic-input");
+  let descriptInput = $("input#description-input");
+  let dateInput = $("input#datepicker");
+  let timeInput = $("input#time");
+  let capacityInput = $("input#capacity-input");
+  let priceInput = $("input#price-input");
+
+  // When the signup button is clicked, we validate the email and password are not blank
+  classForm.on("submit", function (event) {
+    event.preventDefault();
+
+    let classData = {
+      topic: topicInput.val().trim(),
+      description: descriptInput.val().trim(),
+      date: dateInput.val().trim(),
+      time: timeInput.val().trim(),
+      capacity: capacityInput.val().trim(),
+      price: priceInput.val().trim(),
+    };
+
+    console.log("class: ", classData);
+
+    if (!classData.topic || !classData.description || !classData.date ||
+      !classData.time || !classData.capacity || !classData.price === null) {
+      return;
+    }
+    createClass(classData.topic, classData.description, classData.date, classData.time, classData.capacity, classData.price);
     //   topicInput.val("");
     //   descriptInput.val("");
     //   $('input[name=studOrInst]:checked',
     //     '#s').val("");
-    });
+  });
 
   function createClass(topic, description, date, time, capacity, price) {
     $.post("/classes", {
-      topic: topic,
-      description: description,
-      date: date,
-      time: time,
-      capacity: capacity,
-      price: price
-    })
+        topic: topic,
+        description: description,
+        date: date,
+        time: time,
+        capacity: capacity,
+        price: price
+      })
       .then(data => {
         console.log(data);
         // If there's an error, handle it by throwing up a bootstrap alert
       })
-      .catch(error);
+      .catch(error => console.log(error));
   }
 
 });
--- routes/class-routes.js
+++ routes/class-routes.js
@@ -1,13 +1,12 @@
 // This file offers a set of routes for displaying and saving classes data to the db
-
+const isInstructor = require('../config/middleware/isInstructor')
 // Requiring our models
 const db = require("../models");
 
-// Routes
 module.exports = function (app) {
 
     // GET route for getting all of the classes
-    app.get("/classes", (req, res) => {
+    app.get("/classes", isInstructor, (req, res) => {
         let query = {};
         //We use new way to pass optional parameters here: req.query
         //example url: "/api/posts?author_id=1"
@@ -25,9 +24,25 @@ module.exports = function (app) {
     });
 
     // POST route for saving a new class
-    app.post("/classes", (req, res) => {
-        db.Class.create(req.body).then(function (dbClass) {
-            res.json(dbClass);
-        });
+    app.post("/classes", isInstructor, (req, res) => {
+
+        console.log(req.user)
+
+        db.Instructor.findOne({
+            where: {
+                UserId: req.user.id
+            }
+        }).then(dbInstructor => {
+            const newClass = req.body;
+            newClass.InstructorId = dbInstructor.id
+
+            console.log(dbInstructor)
+            console.log(newClass)
+
+            db.Class.create(newClass).then(function (dbClass) {
+                res.json(dbClass);
+            });
+        })
+
     });
-};
+}
