087a901591116e7e01aa660c532198022947f10d
GusGo <gus.gonzalez.15@gmail.com>
2020-03-16 01:31:53 -0400

    added user name in signup all the way to db

--- public/js/instructor.js
+++ public/js/instructor.js
@@ -2,6 +2,6 @@ $(document).ready(function () {
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
   $.get("/api/user_data").then(data => {
-    $(".instructor-name").text(data.email);
+    $(".instructor-name").text(data.name);
   });
 });
