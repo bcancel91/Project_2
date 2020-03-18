4b3e2c2abe97ae6a141bfa87be4c9301fd5b9f63
Daria <dariagnaumova@gmail.com>
2020-03-17 22:18:56 -0400

    getting all classes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,20 +1,35 @@
 // Requiring our custom middleware for checking if a user is logged in
 let isAuthenticated = require("../config/middleware/isAuthenticated");
-
+const db = require("../models");
+let isInstructor = require("../config/middleware/isInstructor")
 module.exports = function (app) {
 
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/instructors", isAuthenticated, (req, res) => {
-    if (!req.user.instructor) {
-      res.send("Can't access this page!");
-    } else {
-
       // here we need to find all classes, sorted by date, and put in hbs object containing the classes
+      db.Class.findAll({
+    }).then(function (dbClass) {
+        // res.json(dbClass);
+        console.log(req.user);
+        // const instructorClasses = dbClass.filter(classItem => {
+        //     return classItem.instructorId === req.user.id;
+        // });
+        // console.log(instructorClasses);
+        
+        dbClassValues = dbClass.map(classObj => {
+          return classObj.dataValues;
+        });
 
+        let hbsObject = {
+            classes: dbClassValues,
+            // instructorClasses: instructorClasses
+        };
+        console.log(hbsObject);
+        res.render("instructors", hbsObject);
+
+    });
       // let hbsObject = {};
-      res.render('instructors');
-    }
   });
 
   app.get("/students", isAuthenticated, (req, res) => {
