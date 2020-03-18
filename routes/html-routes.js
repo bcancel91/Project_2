b9641b46e2022bfc8d1244c1105fb10000cb1191
GusGo <gus.gonzalez.15@gmail.com>
2020-03-18 13:14:56 -0400

    using our new middleware functions to authenticate and give access

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,13 +1,17 @@
 // Requiring our custom middleware for checking if a user is logged in
-let isAuthenticated = require("../config/middleware/isAuthenticated");
+// let isAuthenticated = require("../config/middleware/isAuthenticated");
 const db = require("../models");
-let isInstructor = require("../config/middleware/isInstructor")
+let isInstructor = require("../config/middleware/isInstructor");
+let isStudent = require("../config/middleware/isStudent");
+
 module.exports = function (app) {
 
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
-  app.get("/instructors", isAuthenticated, (req, res) => {
+  app.get("/instructors", isInstructor, (req, res) => {
     // here we need to find all classes, sorted by date, and put in hbs object containing the classes
+
+
     db.Class.findAll({
     }).then(function (dbClass) {
       // res.json(dbClass);
@@ -29,10 +33,12 @@ module.exports = function (app) {
       res.render("instructors", hbsObject);
 
     });
+
+
     // let hbsObject = {};
   });
 
-  app.get("/students", isAuthenticated, (req, res) => {
+  app.get("/students", isStudent, (req, res) => {
 
     db.Class.findAll({
     }).then(function (dbClass) {
