11f04014c27940330ad1ceab4d41e538c467bbda
GusGo <gus.gonzalez.15@gmail.com>
2020-03-18 13:14:00 -0400

    got these middle ware functions working

--- config/middleware/isInstructor.js
+++ config/middleware/isInstructor.js
@@ -1,10 +1,10 @@
 // This is middleware for restricting routes a user is not allowed to visit if not logged in
 module.exports = function (req, res, next) {
     // If the user is logged in, continue with the request to the restricted route
-    if (req.user.instructor) {
+    if (req.user && req.user.instructor) {
         return next();
     }
 
     // If the user isn't logged in, redirect them to the login page
-    return res.redirect("/");
+    return res.send("You cannot access this page! Please go back and try logging in again.");
 };
