47eb868488f942232fde8adca67693f26f670518
Daria <dariagnaumova@gmail.com>
2020-03-19 12:20:18 -0400

    userId - studentId bug fix, + valid to class form

--- routes/student-routes.js
+++ routes/student-routes.js
@@ -12,7 +12,7 @@ module.exports = function (app) {
 
         let studentClass = {
             ClassId: req.body.id,
-            StudentId: req.user.id
+            UserId: req.user.id
         }
 
         // console.log('hit', studentClass);
@@ -22,5 +22,31 @@ module.exports = function (app) {
             res.sendStatus(200);
         });
     });
-};
+
+      // Here we've add our isAuthenticated middleware to this route.
+  // If a user who is not logged in tries to access this route they will be redirected to the signup page
+  app.get("/api/students/myclasses", isStudent, (req, res) => {
+    // here we need to find all classes that belong to this student
+    db.UserClass.findAll({
+    }).then(function (studentClass) {
+      res.json(studentClass);
+      console.log(req.user);
+      // const instructorClasses = dbClass.filter(classItem => {
+      //     return classItem.instructorId === req.user.id;
+      // });
+      // console.log(instructorClasses);
+
+      studentClassVals = studentClass.map(classObj => {
+        return classObj.dataValues;
+      });
+
+      let hbsObject = {
+        classes: studentClassVals,
+      };
+      // console.log(hbsObject);
+      res.render("students", hbsObject);
+
+    });
+});
+}
 
