437e87c849ec130a8289a1de2f3530d482230492
GusGo <gus.gonzalez.15@gmail.com>
2020-03-18 21:44:41 -0400

    THIS CHANGE OMG

--- server.js
+++ server.js
@@ -28,6 +28,7 @@ app.set("view engine", "handlebars");
 require("./routes/api-routes.js")(app);
 require("./routes/instructor-class-routes.js")(app);
 require("./routes/html-routes.js")(app);
+require("./routes/student-routes.js")(app);
 
 // Syncing our database and logging a message to the user upon success
 db.sequelize.sync().then(function () {
