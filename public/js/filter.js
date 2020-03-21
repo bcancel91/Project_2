4c4729f72cb909dbce9dae079fe5c8be8784e964
GusGo <gus.gonzalez.15@gmail.com>
2020-03-21 18:35:09 -0400

    tweaked to check if the user is a student or instructor which informs which url the request is sent to

--- public/js/filter.js
+++ public/js/filter.js
@@ -3,47 +3,57 @@ const setSelected = (el) => {
 }
 
 const applySettings = () => {
+
     let filterSettings = {};
-    let queryURL = "/instructors/filtered?parameters=";
 
-    $("li.selected").each(function () {
+    $.get("/api/user_data").then(user => {
+        let queryURL;
 
-        let setting = $(this).children().data("setting");
-
-        switch (setting) {
-            case "my":
-            case "all": {
-                filterSettings.classes = setting;
-                break;
-            }
-            // d for duration
-            case "d1":
-            case "d2":
-            case "d3": {
-                filterSettings.duration = setting;
-                break;
-            }
-            // p for price
-            case "free":
-            case "p2":
-            case "p3": {
-                filterSettings.price = setting;
-                break;
-            }
-            // if instructor was chosen, store the instructor id in req
-            case "Instructor": {
-                filterSettings.instructor = $(this).children().data("id");
-                break;
-            }
+        if (user.instructor) {
+            queryURL = "/instructors/filtered?parameters=";
+        } else {
+            queryURL = "/students/filtered?parameters=";
         }
+
+        $("li.selected").each(function () {
+
+            let setting = $(this).children().data("setting");
+
+            switch (setting) {
+                case "my":
+                case "all": {
+                    filterSettings.classes = setting;
+                    break;
+                }
+                // d for duration
+                case "d1":
+                case "d2":
+                case "d3": {
+                    filterSettings.duration = setting;
+                    break;
+                }
+                // p for price
+                case "free":
+                case "p2":
+                case "p3": {
+                    filterSettings.price = setting;
+                    break;
+                }
+                // if instructor was chosen, store the instructor id in req
+                case "Instructor": {
+                    filterSettings.instructor = $(this).children().data("id");
+                    break;
+                }
+            }
+        });
+
+        console.log(filterSettings);
+
+        if ($.isEmptyObject(filterSettings)) {
+            return;
+        }
+
+        queryURL += encodeURIComponent(JSON.stringify(filterSettings));
+        window.location.href = queryURL;
     });
-
-    console.log(filterSettings);
-
-    if ($.isEmptyObject(filterSettings)) {
-        return;
-    }
-
-    queryURL += encodeURIComponent(JSON.stringify(filterSettings));
-    window.location.href = queryURL;
 }
