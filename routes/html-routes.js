3585102a2a29cfe20a9fd913a1641fbe4684e9c2
GusGo <gus.gonzalez.15@gmail.com>
2020-03-21 13:54:22 -0400

    cleanup and added filter view route for instructor

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,51 +1,31 @@
 // Requiring our custom middleware for checking if a user is logged in
 // let isAuthenticated = require("../config/middleware/isAuthenticated");
 const db = require("../models");
+const classFilter = require("../lib/classFilter");
 let isInstructor = require("../config/middleware/isInstructor");
 let isStudent = require("../config/middleware/isStudent");
 
 module.exports = function (app) {
 
-  // Here we've add our isAuthenticated middleware to this route.
-  // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/instructors", isInstructor, (req, res) => {
-    // here we need to find all classes, sorted by date, and put in hbs object containing the classes
-
 
     db.Class.findAll({}).then(function (dbClass) {
-      // res.json(dbClass);
-      // console.log(req.user);
-      // const instructorClasses = dbClass.filter(classItem => {
-      //     return classItem.instructorId === req.user.id;
-      // });
-      // console.log(instructorClasses);
 
       dbClassValues = dbClass.map(classObj => {
         return classObj.dataValues;
       });
 
       let hbsObject = {
-        classes: dbClassValues,
-        // instructorClasses: instructorClasses
+        classes: dbClassValues
       };
-      // console.log(hbsObject);
+
       res.render("instructors", hbsObject);
-
     });
-
-
-    // let hbsObject = {};
   });
 
   app.get("/students", isStudent, (req, res) => {
 
     db.Class.findAll({}).then(function (dbClass) {
-      // res.json(dbClass);
-      // console.log(req.user);
-      // const instructorClasses = dbClass.filter(classItem => {
-      //     return classItem.instructorId === req.user.id;
-      // });
-      // console.log(instructorClasses);
 
       dbClassValues = dbClass.map(classObj => {
         return classObj.dataValues;
@@ -54,19 +34,30 @@ module.exports = function (app) {
       let hbsObject = {
         my: false,
         all: true,
-        classes: dbClassValues,
-        // instructorClasses: instructorClasses
+        classes: dbClassValues
       };
-      // console.log(hbsObject);
-      res.render("students", hbsObject);
 
+      res.render("students", hbsObject);
     });
   });
 
+  // instructors filtered view route 
+  app.get("/instructors/filtered/", isInstructor, function (req, res) {
+
+    let filterSettings = JSON.parse(req.query.parameters);
+    console.log(filterSettings);
+
+    classFilter(req.user.id, filterSettings).then(dbClassValues => {
+      res.render("instructors", { classes: dbClassValues });
+    })
+
+  });
+
+  // homepage
   app.get('/', (req, res) => {
 
     res.render('index');
 
   });
 
-};
+}
